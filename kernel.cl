cc1285d07401bc63c8d21ce1ac5ab96c55c545be
Seonkyo Ok <uyw4687@naver.com>
2021-05-25 21:39:00 +0900

    mpi

--- kernel.cl
+++ kernel.cl
@@ -1,10 +1,33 @@
+#define BS (16*32)
+
 __kernel void sgemm(__global float *A, __global float *B, __global float *C, int M, int N, int K) {
-  int i = get_global_id(0);
-  int j = get_global_id(1);
-  if (i >= M || j >= N) return;
-  float result = 0;
-  for (int k = 0; k < K; k++) {
-    result += A[i * K + k] * B[k * N + j];
+  int i = get_global_id(0); // row index of C
+  int j = get_global_id(1); // column index of C
+  if (i >= M || j >= N) return; // boundary check
+
+  float res = 0;
+  if (j<N/BS*BS) {
+    int lj = get_local_id(1);
+    __local float a_[BS];
+
+    for (int bn = 0; bn < K/BS; bn++) {
+      a_[lj] = A[i * K + (bn*BS+lj)];
+      barrier(CLK_LOCAL_MEM_FENCE);
+
+      for(int k=0;k<BS;k++) {
+        res += a_[k] * B[(bn*BS+k) * N + j];
+      }
+      barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    for (int k = K/BS*BS; k < K; k++) {
+      res += A[i * K + k] * B[k * N + j];
+    }
   }
-  C[i * N + j] = result;
+  else {
+    for (int k = 0; k < K; k++) {
+      res += A[i * K + k] * B[k * N + j];
+    }
+  }
+  C[i * N + j] = res;
 }
--- mat_mul.c
+++ mat_mul.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <CL/cl.h>
+#include <mpi.h>
+#include "util.h"
 
 #define CHECK_ERROR(err) \
   if (err != CL_SUCCESS) { \
@@ -22,28 +24,79 @@ static cl_mem a_d[NUM_GPU], b_d[NUM_GPU], c_d[NUM_GPU];
 static cl_uint num_dev;
 static int num_threads = 4;
 
-void mat_mul(float *A, float *B, float *C, int M, int N, int K) {
+void ms(float *A, float *B, float *C, int M, int N, int K, int mpi_rank, int mpi_size) {
+  if (mpi_rank == 0) {
+    for (int i = 1; i < mpi_size-1; ++i) {
+      MPI_Send(A+M/mpi_size*i*K, M/mpi_size * K, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+    }
+    if (mpi_size != 1) {
+      MPI_Send(A+M/mpi_size*(mpi_size-1)*K, M*K-M/mpi_size*(mpi_size-1) * K, MPI_FLOAT, mpi_size-1, 0, MPI_COMM_WORLD);
+    }
+  } else {
+    if (mpi_rank!=mpi_size-1) {
+      MPI_Recv(A, M/mpi_size * K, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, NULL);
+    }
+    else {
+      MPI_Recv(A, M*K-M/mpi_size*mpi_rank * K, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, NULL);
+    }
+  }
+
+  if (mpi_rank == 0) {
+    for (int i = 1; i < mpi_size; ++i) {
+      MPI_Send(B, K * N, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+    }
+  } else {
+    MPI_Recv(B, K * N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, NULL);
+  }
+}
+
+void mr(float *A, float *B, float *C, int M, int N, int K, int mpi_rank, int mpi_size) {
+  if (mpi_rank == 0) {
+    for (int i = 1; i < mpi_size-1; ++i) {
+      MPI_Recv(C+M/mpi_size*i*N, M/mpi_size * N, MPI_FLOAT, i, 0, MPI_COMM_WORLD, NULL);
+    }
+    if (mpi_size != 1) {
+      MPI_Recv(C+M/mpi_size*(mpi_size-1)*N, M*N-M/mpi_size*(mpi_size-1) * N, MPI_FLOAT, mpi_size-1, 0, MPI_COMM_WORLD, NULL);
+    }
+  } else {
+    if (mpi_rank!=mpi_size-1) {
+      MPI_Send(C, M/mpi_size * N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+    }
+    else {
+      MPI_Send(C, M*N-M/mpi_size*mpi_rank * N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+    }
+  }
+}
+
+void mat_mul(float *A, float *B, float *C, int M, int N, int K, int mpi_rank, int mpi_size) {
+  if (mpi_rank != 0) {
+    alloc_mat(&A, M/mpi_size+(mpi_size-1), K);
+    alloc_mat(&B, K, N);
+    alloc_mat(&C, M/mpi_size+(mpi_size-1), N);
+  }
+  ms(A, B, C, M, N, K, mpi_rank, mpi_size);
+
+  int new_M = M/mpi_size;
+  if (mpi_rank==mpi_size-1) {
+    new_M = M/mpi_size+mpi_size-1;
+  }
+
 #pragma omp parallel for num_threads(num_threads)
   for (int i = 0;i < num_dev; i++) {
     if (i!=num_dev-1) {
-      err = clEnqueueWriteBuffer(queues[i], a_d[i], CL_FALSE, M/num_dev*i*K*sizeof(float), M/num_dev * K * sizeof(float), A+M/num_dev*i*K, 0, NULL, NULL);
+      err = clEnqueueWriteBuffer(queues[i], a_d[i], CL_TRUE, 0, new_M/num_dev * K * sizeof(float), A+new_M/num_dev*i*K, 0, NULL, NULL);
     }
     else {
-      err = clEnqueueWriteBuffer(queues[i], a_d[i], CL_FALSE, M/num_dev*i*K*sizeof(float), M * K * sizeof(float) - M/num_dev*i * K * sizeof(float), A+M/num_dev*i*K, 0, NULL, NULL);
+      err = clEnqueueWriteBuffer(queues[i], a_d[i], CL_TRUE, 0, new_M*K*sizeof(float)-new_M/num_dev*i*K * sizeof(float), A+new_M/num_dev*i*K, 0, NULL, NULL);
     }
     CHECK_ERROR(err);
 
-    err = clEnqueueWriteBuffer(queues[i], b_d[i], CL_FALSE, 0, K * N * sizeof(float), B, 0, NULL, NULL);
+    err = clEnqueueWriteBuffer(queues[i], b_d[i], CL_TRUE, 0, K * N * sizeof(float), B, 0, NULL, NULL);
     CHECK_ERROR(err);
   }
 
 #pragma omp parallel for num_threads(num_threads)
-  for (int i = 0; i < num_dev; ++i) {
-    err = clFinish(queues[i]);
-    CHECK_ERROR(err);
-  }
-#pragma omp parallel for num_threads(num_threads)
-  for (int i = 0; i < num_dev; ++i) {
+  for (int i = 0;i < num_dev; i++) {
     err = clSetKernelArg(kernels[i], 0, sizeof(cl_mem), &a_d[i]);
     CHECK_ERROR(err);
     err = clSetKernelArg(kernels[i], 1, sizeof(cl_mem), &b_d[i]);
@@ -58,39 +111,33 @@ void mat_mul(float *A, float *B, float *C, int M, int N, int K) {
     CHECK_ERROR(err);
   }
 
-  size_t gws[2] = {M/num_dev+num_dev-1, N}, lws[2] = {1, 16*16}, off[4][2] = {0,};
+  size_t gws[2] = {new_M/num_dev+num_dev-1, N}, lws[2] = {1, 16*32};
   for (int i = 0; i < 2; ++i) {
     gws[i] = (gws[i] + lws[i] - 1) / lws[i] * lws[i];
   }
 
 #pragma omp parallel for num_threads(num_threads)
-  for (int i = 0; i < num_dev; ++i) {
-    off[i][0] = M/num_dev*i; off[i][1] = 0;
-    err = clEnqueueNDRangeKernel(queues[i], kernels[i], 2, off[i], gws, lws, 0, NULL, NULL);
+  for (int i = 0;i < num_dev; i++) {
+    err = clEnqueueNDRangeKernel(queues[i], kernels[i], 2, NULL, gws, lws, 0, NULL, NULL);
     CHECK_ERROR(err);
   }
 
 #pragma omp parallel for num_threads(num_threads)
-  for (int i = 0; i < num_dev; ++i) {
-    err = clFinish(queues[i]);
-    CHECK_ERROR(err);
-  }
-
-#pragma omp parallel for num_threads(num_threads)
-  for (int i = 0; i < num_dev; ++i) {
+  for (int i = 0;i < num_dev; i++) {
     if (i!=num_dev-1) {
-      err = clEnqueueReadBuffer(queues[i], c_d[i], CL_FALSE, M/num_dev*i*N*sizeof(float), M/num_dev * N * sizeof(float), C+M/num_dev*i*N, 0, NULL, NULL);
+      err = clEnqueueReadBuffer(queues[i], c_d[i], CL_TRUE, 0, new_M/num_dev * N * sizeof(float), C+new_M/num_dev*i*N, 0, NULL, NULL);
     }
     else {
-      err = clEnqueueReadBuffer(queues[i], c_d[i], CL_FALSE, M/num_dev*i*N*sizeof(float), M * N * sizeof(float) - M/num_dev*i*N*sizeof(float), C+M/num_dev*i*N, 0, NULL, NULL);
+      err = clEnqueueReadBuffer(queues[i], c_d[i], CL_TRUE, 0, new_M * N * sizeof(float) - new_M/num_dev*i*N*sizeof(float), C+new_M/num_dev*i*N, 0, NULL, NULL);
     }
     CHECK_ERROR(err);
   }
+  mr(A, B, C, M, N, K, mpi_rank, mpi_size);
 
-#pragma omp parallel for num_threads(num_threads)
-  for (int i = 0; i < num_dev; ++i) {
-    err = clFinish(queues[i]);
-    CHECK_ERROR(err);
+  if (mpi_rank != 0) {
+    free(A);
+    free(B);
+    free(C);
   }
 }
 
@@ -144,18 +191,19 @@ static cl_program create_and_build_program_with_source(cl_context context, cl_de
   return program;
 }
 
-void mat_mul_init(float *A, float *B, float *C, int M, int N, int K) {
+void mat_mul_init(float *A, float *B, float *C, int M, int N, int K, int mpi_rank, int mpi_size) {
   err = clGetPlatformIDs(1, &platform, NULL);
   CHECK_ERROR(err);
+  printf("[rank %d] ", mpi_rank);
   print_platform_info(platform);
 
   err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, NUM_GPU, devices, &num_dev);
+  CHECK_ERROR(err);
   if (num_dev>NUM_GPU) {
     num_dev = NUM_GPU;
   }
-  CHECK_ERROR(err);
   for (int i = 0;i < num_dev; i++) {
-    printf("Devide %d: ", i);
+    printf("[rank %d] Devide %d: ", mpi_rank, i);
     print_device_info(devices[i]);
   }
 
@@ -174,19 +222,33 @@ void mat_mul_init(float *A, float *B, float *C, int M, int N, int K) {
   }
 
   for (int i = 0;i < num_dev; i++) {
-    kernels[i] = clCreateKernel(programs[i], "sgemm", &err);
+    kernels[i] = clCreateKernel(programs[i], "sgemm", &err); 
+    CHECK_ERROR(err);
+  }
+
+  int new_M = M/mpi_size;
+  if (mpi_rank==mpi_size-1) {
+    new_M = M/mpi_size+mpi_size-1;
+  }
+
+  for (int i = 0;i < num_dev; i++) {
+    a_d[i] = clCreateBuffer(contexts[i], CL_MEM_READ_WRITE, (new_M/num_dev+num_dev-1) * K * sizeof(float), NULL, &err);
+    CHECK_ERROR(err);
+    b_d[i] = clCreateBuffer(contexts[i], CL_MEM_READ_WRITE, K * N * sizeof(float), NULL, &err);
+    CHECK_ERROR(err);
+    c_d[i] = clCreateBuffer(contexts[i], CL_MEM_READ_WRITE, (new_M/num_dev+num_dev-1) * N * sizeof(float), NULL, &err);
     CHECK_ERROR(err);
   }
 
   for (int i = 0;i < num_dev; i++) {
-    a_d[i] = clCreateBuffer(contexts[i], CL_MEM_READ_WRITE, M * K * sizeof(float), NULL, &err);
-    CHECK_ERROR(err);
-    b_d[i] = clCreateBuffer(contexts[i], CL_MEM_READ_WRITE, K * N * sizeof(float), NULL, &err);
-    CHECK_ERROR(err);
-    c_d[i] = clCreateBuffer(contexts[i], CL_MEM_READ_WRITE, M * N * sizeof(float), NULL, &err);
+    err = clFinish(queues[i]);
     CHECK_ERROR(err);
   }
 }
 
-void mat_mul_final(float *A, float *B, float *C, int M, int N, int K) {
+void mat_mul_final(float *A, float *B, float *C, int M, int N, int K, int mpi_rank, int mpi_size) {
+  for (int i = 0;i < num_dev; i++) {
+    err = clFinish(queues[i]);
+    CHECK_ERROR(err);
+  }
 }
